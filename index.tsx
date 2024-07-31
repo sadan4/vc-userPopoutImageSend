1e870b0ca51dc83584d5592100ba464c1e89f29f
sadan <117494111+sadan4@users.noreply.github.com>
2024-07-30 23:02:33 -0400

    create plugin UserPopoutImageSend

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import definePlugin from "@utils/types";
+
+definePlugin({
+    name: "UserPopoutImageSend",
+    description: "",
+    authors: [
+        {
+            name: "sadan",
+            id: 521819891141967883n
+        }
+    ]
+})
